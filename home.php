3da6cb8620bc29df929d7428a1511d02a7756cbb
Mohammed Hammed <hammedmhd@gmail.com>
2017-02-09 00:34:02 +0800

    Auto home page on load

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+require 'functions.php';
+
+echo "<p class='button' name='Stock Items' url='items.php' onclick='loadthis(this)'>Stock Items</p>";
+
+echo "<p class='button' name='Orders' url='orders.php' onclick='loadthis(this)'>Orders</p>";
+
+
+?>
